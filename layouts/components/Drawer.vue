a97053acc9545726d38150374b90019e9ed0b4db
piemekanika <piemekanika@hotmail.com>
2019-08-25 13:49:06 +0700

    created a boilerplate for drawer

---
+++ layouts/components/Drawer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="drawer-outer">
+    <div class="drawer">
+      Winx la Notte
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.drawer-outer {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vw;
+}
+</style>
