9fbec40d5ea7cb05fbdf3804b65339c6f7e95c56
piemekanika <piemekanika@hotmail.com>
2019-08-25 18:02:13 +0700

    added yaml loader

--- nuxt.config.js
+++ nuxt.config.js
@@ -56,7 +56,11 @@ export default {
     /*
     ** You can extend webpack config here
     */
-    extend (config, ctx) {
+    extend(config, ctx) {
+      config.module.rules.push({
+        test: /\.ya?ml$/,
+        use: 'js-yaml-loader',
+      })
     }
   }
 }
